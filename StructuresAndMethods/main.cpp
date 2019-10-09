4adae17182f0c043239950dbe087ac7f4af005a8
Victoria Vilas Boas <vick_vilasb@outlook.com>
2019-10-09 13:26:26 -0300

    improving main

--- StructuresAndMethods/main.cpp
+++ StructuresAndMethods/main.cpp
@@ -6,5 +6,20 @@
 using namespace std;
 
 int main() {
-
+    int value;
+    Tree * tree = new Tree;
+    char option;
+    while(cin >> option)
+    {
+        if (option == 'i')
+        {
+            cin >> value;
+            tree->insert_in_order(value);
+        } 
+        else if (option == 'p')
+        {
+            tree->print_tree();
+        }
+    }
+    return 0;
 }
